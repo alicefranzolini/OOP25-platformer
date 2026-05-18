bd59810328e2a9ef0567d0a69800123a6aa8b83b
Lorenzo Vasini <vasinil384@gmail.com>
2026-05-18 15:44:38 +0200

    fix javafx app entrypoint

--- src/main/java/it/unibo/platformer/Main.java
+++ src/main/java/it/unibo/platformer/Main.java
@@ -1,11 +1,13 @@
 package it.unibo.platformer;
 
+import javafx.application.Application;
+
 public final class Main {
 
     private Main() {
     }
 
     public static void main(final String[] args) {
-        App.main(args);
+        Application.launch(App.class, args);
     }
 }
