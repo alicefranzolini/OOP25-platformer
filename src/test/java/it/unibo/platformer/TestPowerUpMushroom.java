fc9b2f3be7f6b422be82a2abed6bd589ed50121f
Sabrina Magi <sabrina.magi3@studio.unibo.it>
2026-06-13 18:32:38 +0200

    fix mushroom test

--- src/test/java/it/unibo/platformer/TestPowerUpMushroom.java
+++ src/test/java/it/unibo/platformer/TestPowerUpMushroom.java
@@ -1,21 +1,30 @@
 package it.unibo.platformer;
 
 import it.unibo.platformer.model.entities.powerup.MushroomPowerUp;
-import it.unibo.platformer.model.physics.api.BasicPhysics;
 import it.unibo.platformer.model.entities.players.PlayerImpl;
+import it.unibo.platformer.model.physics.api.BasicPhysics;
+import it.unibo.platformer.model.physics.impl.BasicPhysicsImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Tests for MushroomPowerUp: applyEffect -> BIG.
  */
 class TestPowerUpMushroom {
-    BasicPhysics physics ;
+
+    private BasicPhysics physics;
+
+    @BeforeEach
+    void setup() {
+        physics = new BasicPhysicsImpl();
+    }
+
     @Test
     void applyEffectSetsPlayerBig() {
-        MushroomPowerUp mushroom = new MushroomPowerUp(0, 0, physics);
-        PlayerImpl p = new PlayerImpl(0, 0, physics);
+        final MushroomPowerUp mushroom = new MushroomPowerUp(0, 0, physics);
+        final PlayerImpl p = new PlayerImpl(0, 0, physics);
 
         mushroom.applyEffect(p);
 
