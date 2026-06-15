eac274bcdc0617e01f4e047fddda374e25a1b2cf
Gabriele <gabriele.fortarezza@studio.unibo.it>
2026-06-15 17:11:02 +0200

    fixed bugs

--- src/main/java/it/unibo/platformer/model/physics/api/BasicPhysics.java
+++ src/main/java/it/unibo/platformer/model/physics/api/BasicPhysics.java
@@ -2,6 +2,6 @@ package it.unibo.platformer.model.physics.api;
 
 public interface BasicPhysics {
     
-    public void UpdatePosition(GameObject obj, double deltaTime);
+    public void updatePosition(GameObject obj, double deltaTime);
 
 }
