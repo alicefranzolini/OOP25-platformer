4b6cebd93ec263cb5702a34e806cbb7c71bffd52
Gabriele <gabriele.fortarezza@studio.unibo.it>
2026-06-16 12:18:35 +0200

    created physics test's folder and added TEstPhysics

--- src/test/java/it/unibo/platformer/model/physics/TestPhysics.java
+++ src/test/java/it/unibo/platformer/model/physics/TestPhysics.java
@@ -1,4 +1,4 @@
-package it.unibo.platformer;
+package it.unibo.platformer.model.physics;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
