1c5651d40f67f69ba5690b0166f1be40440e5ea9
Sabrina Magi <sabrina.magi3@studio.unibo.it>
2026-06-23 22:17:32 +0200

    no side expception

--- src/main/java/it/unibo/platformer/model/physics/NoSideException.java
+++ src/main/java/it/unibo/platformer/model/physics/NoSideException.java
@@ -6,6 +6,7 @@ import java.io.IOException;
  * This is the exception if there isn't sides.
  */
 public class NoSideException extends IOException {
+    private static final long serialVersionUID = 1L;
     private static final String MSG = "No side found";
 
     /**
